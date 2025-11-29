a26a907b365e6647284b3221211283d966e28e55
Yura Yurtsiv <mryurtsiv@gmail.com>
2025-11-29 19:47:50 +0200

    fixes

--- src/main/java/ua/dager/adventcalendar/config/JsonConfigRepository.java
+++ src/main/java/ua/dager/adventcalendar/config/JsonConfigRepository.java
@@ -3,18 +3,12 @@ package ua.dager.adventcalendar.config;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.jetbrains.annotations.Nullable;
-import ua.dager.adventcalendar.AdventCalendar;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 public class JsonConfigRepository {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
@@ -22,8 +16,9 @@ public class JsonConfigRepository {
 
     private Config config;
 
-    public JsonConfigRepository(Path filePath) {
+    public JsonConfigRepository(Path filePath, InputStream default_config) {
         this.filePath = filePath;
+        this.createdDefaultConfig(default_config);
         this.load();
     }
 
@@ -43,35 +38,21 @@ public class JsonConfigRepository {
         return this.config.rewards.get(Integer.toString(day));
     }
 
+    private void createdDefaultConfig(InputStream file) {
+        if (Files.exists(filePath)) return;
+        try {
+            Files.createDirectories(filePath.getParent());
+            Files.write(filePath, file.readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create default config at " + filePath, e);
+        }
+    }
+
     public void load() {
         try {
-            if (!Files.exists(filePath)) {
-                Files.createDirectories(filePath.getParent());
-                File zipPath = new File("polymer/resource_pack.zip");
-
-                String target = "assets/advent-calendar/demo-configs/calendar_config.json";
-
-                try (ZipFile zipFile = new ZipFile(zipPath)) {
-                    ZipEntry entry = zipFile.getEntry(target);
-
-                    if (entry == null) {
-                        throw new FileNotFoundException("File not found in ZIP: " + target);
-                    }
-
-                    try (InputStream is = zipFile.getInputStream(entry)) {
-                        String content = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-                        Files.writeString(filePath, content);
-                    }
-
-                } catch (IOException e) {
-                    throw new RuntimeException("Failed to copy calendar config from resource pack");
-                }
-            }
-
             var content = Files.readString(filePath);
             var parsed = GSON.fromJson(content, Config.class);
             if (parsed != null) config = parsed;
-
         } catch (IOException e) {
             throw new RuntimeException("Failed to load JSON data at " + filePath, e);
         }
--- src/main/java/ua/dager/adventcalendar/config/ModConfigs.java
+++ src/main/java/ua/dager/adventcalendar/config/ModConfigs.java
@@ -1,17 +1,35 @@
 package ua.dager.adventcalendar.config;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import ua.dager.adventcalendar.AdventCalendar;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.zip.ZipFile;
 
 public class ModConfigs {
     public static void register() {
-        AdventCalendar.configRepo = new JsonConfigRepository(Paths.get("advent-calendar/calendar_config.json"));
         AdventCalendar.claimedGiftsRepo = new JsonClaimedGiftsRepository(
-            Paths.get("advent-calendar/claimed_gifts.json")
+            Paths.get("config/advent-calendar/claimed_gifts.json")
         );
+
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            var container = FabricLoader.getInstance().getModContainer(AdventCalendar.MOD_ID).orElseThrow();
+            try {
+                var modJarPath = container.getOrigin().getPaths().getFirst();
+                var zip = new ZipFile(modJarPath.toFile());
+                var entry = zip.getEntry("assets/advent-calendar/demo-configs/calendar_config.json");
+                var in = zip.getInputStream(entry);
+                AdventCalendar.configRepo = new JsonConfigRepository(
+                    Paths.get("config/advent-calendar/calendar_config.json"),
+                    in
+                );
+                zip.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
             try {
