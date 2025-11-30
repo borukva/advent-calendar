59575716402438271d60d48ba1ec7aa156ca6c19
Yura Yurtsiv <mryurtsiv@gmail.com>
2025-12-01 00:45:54 +0200

    do not allow guests to use command

--- src/main/java/ua/dager/adventcalendar/command/AdventCalendarMainCommand.java
+++ src/main/java/ua/dager/adventcalendar/command/AdventCalendarMainCommand.java
@@ -1,12 +1,24 @@
 package ua.dager.adventcalendar.command;
 
+import com.mojang.authlib.GameProfile;
 import ua.dager.adventcalendar.gui.GuiAdventCalendar;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 
 public class AdventCalendarMainCommand {
     public static int run(CommandContext<CommandSourceStack> context) {
-        GuiAdventCalendar gui = new GuiAdventCalendar(context.getSource().getPlayer());
+        var player = context.getSource().getPlayer();
+        if (player == null) return 0;
+        // If player is a guest
+        if (
+            !context.getSource()
+            .getServer()
+            .getPlayerList()
+            .getWhiteList()
+            .isWhiteListed(new GameProfile(player.getUUID(), player.getName().getString()))
+        ) return 0;
+
+        GuiAdventCalendar gui = new GuiAdventCalendar(player);
         gui.open();
         return 0;
     }
